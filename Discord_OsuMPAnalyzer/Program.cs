c6f0f161a9ae66c97741fe4618c1e18fa62238fe
Marko Kardum <blade1234@web.de>
2018-06-07 00:10:45 +0200

    update

--- Discord_OsuMPAnalyzer/Program.cs
+++ Discord_OsuMPAnalyzer/Program.cs
@@ -46,7 +46,6 @@ namespace Discord_OsuMPAnalyzer
                         _DClient = new DiscordClient(dconfig);
 
                         _DClient.SetWebSocketClient<WebSocket4NetClient>();
-                        //}
 
                         await _DClient.ConnectAsync();
 
@@ -64,63 +63,63 @@ namespace Discord_OsuMPAnalyzer
                         {
                             Console.WriteLine("{0} : {1}", e.EventName, e.Exception);
                         };
-                        //#pragma warning disable
-                        //                    _DClient.MessageCreated += async e =>
-                        //                    {
-                        //                        if (e.Author == _DClient.CurrentUser) Task.Run(() =>
-                        //                        {
-                        //                            MessageHandler MH = new MessageHandler();
-                        //                            MH.NewMessage(e);
-                        //                        }
-                        //                        );
-                        //                    };
-                        //#pragma warning restore
-
-                        Analyze_Format.Analyzed.MultiMatch ToAnalyze = new Analyze_Format.Analyzed.MultiMatch();
-
-                        Analyze_Format.Analyzer.MultiplayerMatch MPMatch = new Analyze_Format.Analyzer.MultiplayerMatch();
-                        MPMatch.MPJson = API.OsuApi.GetMatch(42788258);
-                        ToAnalyze = MPMatch.Analyze(MPMatch.MPJson);
-
-                        foreach (string s in ToAnalyze.AnalyzedData) Console.WriteLine(s);
-
-
-                        List<string> toWrite = new List<string>();
-
-                        foreach (KeyValuePair<ulong, DiscordGuild> dguildKvp in _DClient.Guilds)
+#pragma warning disable
+                        _DClient.MessageCreated += async e =>
                         {
-                            DiscordGuild dguild = dguildKvp.Value;
+                            if (e.Author == _DClient.CurrentUser) Task.Run(() =>
+                            {
+                                MessageHandler MH = new MessageHandler();
+                                MH.NewMessage(e);
+                            }
+                            );
+                        };
+                        #pragma warning restore
 
-                            string output = string.Format("Guild {0} : Owner: {1} Channels: {2} Members: {3}", dguild.Name, dguild.Owner, dguild.Channels, dguild.MemberCount);
-                            Console.WriteLine(DateTime.UtcNow + " -->> " + output);
-                            toWrite.Add(output);
-                            foreach (DiscordChannel dchannel in dguild.Channels)
-                            {
-                                output = string.Format("------Channel {0}: Id: {1} GuildId: {2} channelType: {3}", dchannel.Name, dchannel.Id, dchannel.GuildId, dchannel.Type);
-                                Console.WriteLine(DateTime.UtcNow + " -->> " + output);
-                                toWrite.Add(output);
-                            }
-                            foreach (DiscordMember dmember in dguild.Members)
-                            {
-                                string roles = "";
-                                output = string.Format("------------User {0}: DisplayName: {1} MemberId: {2} Roles: {3}", dmember.Username, dmember.DisplayName, dmember.Id, roles);
-                                Console.WriteLine(DateTime.UtcNow + " -->> " + output);
-                                foreach (DiscordRole drole in dmember.Roles) roles += string.Format(" | {0} id: {1}", drole.Name, drole.Id);
-                                toWrite.Add(output);
-                            }
-                            foreach (DiscordRole drole in dguild.Roles)
-                            {
-                                output = string.Format("------------------Role {0} : {1}", drole.Name, drole.Id);
-                                Console.WriteLine(output);
-                                toWrite.Add(output);
-                            }
-                        }
-                        Console.WriteLine("Saving output to {0}....", Directory.GetCurrentDirectory());
+                        //Analyze_Format.Analyzed.MultiMatch ToAnalyze = new Analyze_Format.Analyzed.MultiMatch();
 
-                        using (StreamWriter sw = new StreamWriter(Directory.GetCurrentDirectory() + @"\botinfo.info", false))
-                        {
-                            foreach (string s in toWrite) sw.WriteLine(s);
-                        }
+                        //Analyze_Format.Analyzer.MultiplayerMatch MPMatch = new Analyze_Format.Analyzer.MultiplayerMatch();
+                        //MPMatch.MPJson = API.OsuApi.GetMatch(42788258);
+                        //ToAnalyze = MPMatch.Analyze(MPMatch.MPJson);
+
+                        //foreach (string s in ToAnalyze.AnalyzedData) Console.WriteLine(s);
+
+
+                        //List<string> toWrite = new List<string>();
+
+                        //foreach (KeyValuePair<ulong, DiscordGuild> dguildKvp in _DClient.Guilds)
+                        //{
+                        //    DiscordGuild dguild = dguildKvp.Value;
+
+                        //    string output = string.Format("Guild {0} : Owner: {1} Channels: {2} Members: {3}", dguild.Name, dguild.Owner, dguild.Channels, dguild.MemberCount);
+                        //    Console.WriteLine(DateTime.UtcNow + " -->> " + output);
+                        //    toWrite.Add(output);
+                        //    foreach (DiscordChannel dchannel in dguild.Channels)
+                        //    {
+                        //        output = string.Format("------Channel {0}: Id: {1} GuildId: {2} channelType: {3}", dchannel.Name, dchannel.Id, dchannel.GuildId, dchannel.Type);
+                        //        Console.WriteLine(DateTime.UtcNow + " -->> " + output);
+                        //        toWrite.Add(output);
+                        //    }
+                        //    foreach (DiscordMember dmember in dguild.Members)
+                        //    {
+                        //        string roles = "";
+                        //        output = string.Format("------------User {0}: DisplayName: {1} MemberId: {2} Roles: {3}", dmember.Username, dmember.DisplayName, dmember.Id, roles);
+                        //        Console.WriteLine(DateTime.UtcNow + " -->> " + output);
+                        //        foreach (DiscordRole drole in dmember.Roles) roles += string.Format(" | {0} id: {1}", drole.Name, drole.Id);
+                        //        toWrite.Add(output);
+                        //    }
+                        //    foreach (DiscordRole drole in dguild.Roles)
+                        //    {
+                        //        output = string.Format("------------------Role {0} : {1}", drole.Name, drole.Id);
+                        //        Console.WriteLine(output);
+                        //        toWrite.Add(output);
+                        //    }
+                        //}
+                        //Console.WriteLine("Saving output to {0}....", Directory.GetCurrentDirectory());
+
+                        //using (StreamWriter sw = new StreamWriter(Directory.GetCurrentDirectory() + @"\botinfo.info", false))
+                        //{
+                        //    foreach (string s in toWrite) sw.WriteLine(s);
+                        //}
 
                         await Task.Delay(-1);
                     }
